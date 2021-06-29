1130da9a491c0ecd321acfce72bafb5e86843b8d
steven chen <schen0096@gmail.com>
2021-06-29 03:22:55 -0400

    commit tree fe1fce7c4bb4eef604b629c8388969f1f6c33c75

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = (dArray) => dArray.slice(0,2)
+
+returnFirstTwoDrivers(drivers)
+
+const returnLastTwoDrivers = (dArray) => dArray.slice(2,4)
+
+returnLastTwoDrivers(drivers)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => (fare) => int * fare
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (dArray, dFunction) => dFunction(dArray)
